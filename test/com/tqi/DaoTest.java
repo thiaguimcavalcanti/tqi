8b5e25265aab3f9cbcfa8f938e18d70cfc3f73c8
thiagocavalcantireis <thiagocavalcantireis@gmail.com>
2013-03-10 05:38:53 -0300

    Melhorias no teste

--- test/com/tqi/DaoTest.java
+++ test/com/tqi/DaoTest.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import org.hibernate.SessionFactory;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -22,8 +23,8 @@ import com.tqi.service.VoteService;
 import com.tqi.util.exceptions.AppException;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"classpath:/hibernateTestConfig.xml"})
-@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = true)
+@ContextConfiguration(locations={"classpath:/tests-config.xml"})
+@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = false)
 @Transactional
 public class DaoTest {
 
@@ -36,12 +37,15 @@ public class DaoTest {
 	@Autowired
 	private MascotService mascotService;
 	
-	// Send using 10 concurrent threads
+	// Send using 2 concurrent threads
     private ExecutorService executor = Executors.newFixedThreadPool(2);
     
     private int count = 0;
     
     private long tempoInicial;
+    
+    @Autowired
+	private SessionFactory sessionFactory;
 	
 	@Before
 	public void before() {
@@ -55,24 +59,55 @@ public class DaoTest {
 	
 	@Test
 	@Transactional(rollbackFor={AppException.class})
-    public void testGetName() {
+	public void testPopulateMascots() {
+		try {
+			MascotBean mascot1 = new MascotBean();
+			mascot1.setName("Amijubi");
+			mascot1.setDescription("União das palavras amizade e jubilo, que está ligado ao tupi-guarani, em que jubi significa amarelo, cor predominante no mascote.");
+			
+			mascotService.save(mascot1);
+			
+			MascotBean mascot2 = new MascotBean();
+			mascot2.setName("Fuleco");
+			mascot2.setDescription("Uma mistura de futebol e ecologia. O nome busca incentivar o cuidado das pessoas com o meio ambiente.");
+			
+			mascotService.save(mascot2);
+			
+			MascotBean mascot3 = new MascotBean();
+			mascot3.setName("Zuzeco");
+			mascot3.setDescription("Mistura da cor azul com ecologia, que busca também incentivar cuidados relacionados à ecologia.");
+			
+			mascotService.save(mascot3);
+			
+		} catch (AppException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	@Transactional(rollbackFor={AppException.class})
+    public void testVotation() {
 
 		tempoInicial = System.currentTimeMillis();
 
-		for (int i=0; i<500; i++) {
+		for (int i=1; i <= 500; i++) {
 			executor.execute(new Runnable() {
 				public void run() {
 					try {
+						
 						count++;
 						
-						if (count >= 500) {
+						// Encerra em 500 e verifica o tempo de execucao
+						if (count >= 497) {
 							long tempoFinal = System.currentTimeMillis();
 							
-							System.out.println("TEMPO DE EXECUCAO: " + (tempoFinal - tempoInicial) );
-					        return;
+							System.out.println("TEMPO DE EXECUCAO: " + (tempoFinal - tempoInicial) + " milisegundos ou "
+									+ ((tempoFinal - tempoInicial) / 1000) + " segundos");
 						}
 						
-						MascotBean mascot = mascotService.findOne(1 + (int)(Math.random() * 2));
+						// Cria um objeto mascote com o item selecionado
+						MascotBean mascot = new MascotBean();
+						mascot.setMascotID(1 + (int)(Math.random() * 2));
 						
 						// Popula o bean
 						VoteBean vote = new VoteBean();
@@ -82,8 +117,6 @@ public class DaoTest {
 						// Salva
 						voteService.save(vote);
 						
-						System.out.println(count);
-
 					} catch (AppException e) {
 						e.printStackTrace();
 					}
@@ -92,8 +125,8 @@ public class DaoTest {
 		}
 
 		try {
+			Thread.sleep(60000);
 			executor.shutdown();
-			Thread.sleep(2000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
